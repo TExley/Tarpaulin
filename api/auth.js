83030dd35ae8dcf615a2b4d82b7d6ddd28d4f5cc
bazarkua <bazarkua@oregonstate.edu>
2023-06-13 19:27:04 -0700

    Update on users, added a user authenication, modified user schema, added users.js endpoints

---
+++ api/auth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const secretKey = "SuperSecret";
+
+async function verifyUser(req, res, next) {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  const userId = req.body.userId || req.body.ownerId || req.params.id;
+  let decoded;
+
+  if (!token) {
+    return res.status(401).json({ error: "Authorization token must be provided" });
+  }
+
+  try {
+    decoded = jwt.verify(token, secretKey);
+
+    if (decoded.id !== userId && decoded.role !== 'admin' && decoded.role !== 'instructor') {
+      return res.status(403).json({ error: "Not authorized" });
+    }
+
+    req.user = decoded; // Add decoded payload to request
+    next();
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+    next(e);
+  }
+}
+
+module.exports = {
+  verifyUser,
+  secretKey
+};
--- api/users.js
+++ api/users.js
@@ -1,32 +1,116 @@
-const { Router } = require('express')
+const { Router } = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Types;
+const User = require("../models/user");
+const Course = require("../models/course");
+const Submission = require("../models/submission");
+const { verifyUser } = require("./auth.js");
+const { secretKey } = require('./auth.js');
 
-const router = Router()
+const router = Router();
 
-router.post('/', (req, res, next) => {
-    try {
-        console.log("  -- Create a new user")
-        res.status(200).send()
-    } catch (err) {
-        next(err)
+router.get("/", verifyUser, async function (req, res, next) {
+  try {
+    const users = await User.find().select("-password").exec();
+    res.status(200).json({ users });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", verifyUser, async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+  if (req.user.role !== 'admin' || (role !== 'admin' && role !== 'instructor')) {
+    return res.status(403).json({ error: "Not authorized." });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ error: "A user with this email already exists." });
     }
-})
 
-router.post('/login', (req, res, next) => {
-    try {
-        console.log("  -- Login a user")
-        res.status(200).send()
-    } catch (err) {
-        next(err)
+    const user = new User({ name, email, password, role });
+    await user.save();
+
+    res.status(201).json({ id: user.id });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.validatePassword(password))) {
+      return res.status(401).json({ error: "Invalid email or password." });
     }
-})
 
-router.get('/:id', (req, res, next) => {
-    try {
-        console.log(`  -- Fetch data about a specific user: ${req.params.id}`)
-        res.status(200).send()
-    } catch (err) {
-        next(err)
+    const payload = {
+      id: user.id,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, secretKey, { expiresIn: "24h" });
+
+    res.status(200).json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", verifyUser, async (req, res, next) => {
+  const userId = req.params.id;
+
+  if (req.user.id !== userId) {
+    return res.status(403).json({ error: "Not authorized." });
+  }
+
+  try {
+    const user = await User.findById(userId).select("-password").exec();
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
     }
-})
 
-module.exports = router
+    res.status(200).json({ user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id/courses", verifyUser, async function (req, res, next) {
+  const userId = req.params.id;
+  
+  try {
+    let userCourses;
+    if(req.user.role === 'instructor') {
+      userCourses = await Course.find({ instructorId: new ObjectId(userId) }).exec();
+    } else if(req.user.role === 'student') {
+      userCourses = await Course.find({ students: new ObjectId(userId) }).exec();
+    } else {
+      return res.status(403).json({ error: "Not authorized." });
+    }
+    res.status(200).json({ courses: userCourses });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id/submissions", verifyUser, async function (req, res, next) {
+  const userId = req.params.id;
+  
+  try {
+    const userSubmissions = await Submission.find({
+      userId: new ObjectId(userId),
+    }).exec();
+    res.status(200).json({ submissions: userSubmissions });
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
